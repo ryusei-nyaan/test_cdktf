2a20b8e2f07351318e36070b6b21e1070b4a614b
ryusei-nyaan <r.s.iwasaki@ezweb.ne.jp>
2025-06-06 00:44:45 +0900

    feat:ネットワークインフラについてのテンプレートの最初作成

---
+++ infra/network/network.ts
@@ -0,0 +1,10 @@
+import { Construct } from "constructs";
+
+class NetworkStack extends Construct {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    // define resources here
+  }
+}
+export default NetworkStack;
