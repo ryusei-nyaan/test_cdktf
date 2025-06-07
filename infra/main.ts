3aee1b57a4c733af2d591340994cbffe519ba49a
ryusei-nyaan <r.s.iwasaki@ezweb.ne.jp>
2025-06-07 16:54:49 +0900

    feat:main.ts側追記

--- infra/main.ts
+++ infra/main.ts
@@ -1,11 +1,27 @@
 import { Construct } from "constructs";
 import { App, TerraformStack } from "cdktf";
+import { AwsProvider } from "@cdktf/provider-aws/lib/provider";
+import NetworkStack from "./network/network";
+import ServerStack from "./server/server";
+import { awsconfig } from "./config";
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
     // define resources here
+    new AwsProvider(this, awsconfig.provider, {
+      region: awsconfig.region,
+      accessKey: awsconfig.accessKey,
+      secretKey: awsconfig.secretKey,
+    });
+    const networkStack = new NetworkStack(this, "network");
+    new ServerStack(
+      this,
+      "server",
+      networkStack.publicENI.id,
+      networkStack.privateENI.id
+    );
   }
 }
 
